f581d286016886695361696d4e46665b6db0903d
haileyanderton@gmail.com <hka122095!>
2017-03-31 21:49:40 -0400

    final commit

--- altmix.py
+++ altmix.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template
 from mix import ALT_MIX # from data.py
 app = Flask(__name__)
 
-
-# define two functions to be used by the routes
-
-# retrieve all the ids from the dataset and put them into a list
 def get_ids_and_songs(source):
     ids_and_songs = []
     for row in source:
@@ -14,8 +10,7 @@ def get_ids_and_songs(source):
         ids_and_songs.append( [id, song] )
     return ids_and_songs
 
-# find the row that matches the id in the URL, retrieve name and photo
-def get_song(source, id): # data source(student.html), id
+def get_song(source, id):
     for row in source:
         if id == str( row["ID"] ):
             song = row["Song"]
@@ -36,7 +31,6 @@ def index():
     return render_template('index.html', pairs=ids_and_songs)
 
 
-# your code here
 @app.route('/song/<id>')
 def song(id):
     # run function to get song data based on the id in the path
@@ -44,6 +38,5 @@ def song(id):
     # pass all the data for the selected song to the template
     return render_template('song.html', id=id, song=song, artist=artist, album=album)
 
-# keep this as is
 if __name__ == '__main__':
     app.run(debug=True)
